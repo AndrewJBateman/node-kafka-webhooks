214a71c9f16df9227ffdae05d968e63b5d65dd98
AndrewJBateman <gomezbateman@yahoo.com>
2022-05-12 12:17:27 +0200

    feat: getting started code added

---
+++ util.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const readline = require("readline");
+
+function readAllLines(path) {
+  return new Promise((resolve, reject) => {
+    try {
+      fs.accessSync(path, fs.constants.R_OK);
+    } catch (err) {
+      reject(err);
+    }
+
+    let lines = [];
+
+    const reader = readline.createInterface({
+      input: fs.createReadStream(path),
+      crlfDelay: Infinity,
+    });
+
+    reader
+      .on("line", (line) => lines.push(line))
+      .on("close", () => resolve(lines));
+  });
+}
+
+exports.configFromPath = async function configFromPath(path) {
+  const lines = await readAllLines(path);
+
+  const filteredLines = lines
+    .filter((line) => !/^\s*?#/.test(line))
+    .map((line) => line.split("=").map((s) => s.trim()))
+    .reduce((config, [k, v]) => {
+      config[k] = v;
+      return config;
+    }, {});
+  console.log("configFromPath output: ", filteredLines);
+  return filteredLines;
+};
